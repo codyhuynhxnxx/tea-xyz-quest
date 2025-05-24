85e03a6244c15788323e6f777de46d38a8b5ed02
codyhuynhxnxx <111124268+codyhuynhxnxx@users.noreply.github.com>
2025-05-24 16:31:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-require('dotenv').config();
+const postRoutes = require('./routes/posts');
 
+const app = express();
 app.use(bodyParser.json());
 
-app.post('/run', (req, res) => {
-  const { userId, distance, duration, locationHash } = req.body;
+app.use('/api', postRoutes);
 
-  // Giả lập ghi dữ liệu lên peaq
-  console.log(`User ${userId} ran ${distance} km in ${duration} minutes.`);
-
-  // TODO: Ghi dữ liệu lên blockchain qua peaq SDK (sẽ bổ sung sau)
-
-  res.json({
-    message: 'Run data received!',
-    reward: distance * 1, // 1 token mỗi km
-  });
+app.listen(3000, () => {
+  console.log('Server đang chạy trên cổng 3000');
 });
-
-app.listen(3000, () => console.log('Server running on port 3000'));
